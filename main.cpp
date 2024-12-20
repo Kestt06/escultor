98c3ec9d9a685f9833ad5b5f87c3e1aae51ea116
Pedro Santos <minishp16@gmail.com>
2024-12-20 17:35:03 -0300

    .

--- main.cpp
+++ main.cpp
@@ -3,29 +3,8 @@
 
 int main(void){
     Sculptor teste(30,30,30);
-    teste.setColor(0.5,0,1,1);
-    teste.putVoxel(29,29,29);
-    teste.putEllipsoid(20,25,20,10,3,3);
-    teste.putBox(0,3,0,3,0,3);
-    teste.cutBox(2,3,2,3,2,3);
-    teste.putSphere(25,9,9,3);
-    teste.cutSphere(20,25,20,3);
-    teste.setColor(1,0,0,0.1);
-    teste.putBox(8,18,8,15,8,18);
-    teste.setColor(0.5,0,1,1);
-    teste.cutEllipsoid(13,13,13,5,6,2);
-    teste.cutVoxel(0,0,0);
-    teste.writeOFF("teste.off");
-
-    Sculptor random(50,50,50);
-    for (int i=0;i<3000;i++){
-        float randomr = static_cast<float>(std::rand()) / RAND_MAX;
-        float randomg = static_cast<float>(std::rand()) / RAND_MAX;
-        float randomb = static_cast<float>(std::rand()) / RAND_MAX;
-        float randoma = static_cast<float>(std::rand()) / RAND_MAX;
-        random.setColor(randomr,randomg,randomb,randoma);
-        random.putVoxel(std::rand() % 51,std::rand() % 51,std::rand() % 51);
-    }
-    random.writeOFF("random.off");
+    teste.setColor(0,0,1,1);
+    teste.putBox(0,10,0,10,0,10);
+    teste.writeOFF("alo.off");
     return 0;
 }
